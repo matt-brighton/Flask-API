0771e3aa14569c4329fc6611c4fb9d2fa3b801eb
matt-brighton <matthew.brighton@hmrc.gov.uk>
2023-12-20 10:17:46 +0000

    commit4

--- api_formulapp/views.py
+++ api_formulapp/views.py
@@ -1,45 +1,57 @@
-from flask import render_template, Blueprint
+import json
+import logging
 import requests
-from flask import json
+from flask import render_template, Blueprint
 
 views = Blueprint('views', __name__)
+
+API_BASE_URL = 'http://ergast.com/api/f1/'
+
+
+def get_data_from_api(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return json.loads(response.content)
+    except requests.RequestException as e:
+        logging.error(f"Error fetching data from {url}: {str(e)}")
+        raise  # Propagate the exception for the caller to handle
 
 
 @views.route('/', methods=['GET'])
 def index():
-    season_race_data = requests.get('http://ergast.com/api/f1/2024.json?limit=1000')
-    total_seasons_data = requests.get('http://ergast.com/api/f1/seasons.json?limit=1000')
-    season_race_data_json = json.loads(season_race_data.content)
-    total_seasons_data_json = json.loads(total_seasons_data.content)
-    season_year = season_race_data_json['MRData']['RaceTable']['season']
-    total_races = season_race_data_json['MRData']['total']
-    all_seasons = []
-    races = []     
+    try:
+        season_race_data_json = get_data_from_api(
+            API_BASE_URL + '2024.json?limit=1000')
+        total_seasons_data_json = get_data_from_api(
+            API_BASE_URL + 'seasons.json?limit=1000')
 
-    for seasons in total_seasons_data_json['MRData']['SeasonTable']['Seasons']:
-        all_season_year = seasons['season']
-        
-        all_seasons.append({
-        'seasons': all_season_year
-        })
-    
-    for race in season_race_data_json['MRData']['RaceTable']['Races']:
-        round_number = race['round']
-        race_name = race['raceName']
-        circuit_name = race['Circuit']['circuitName']
-        country = race['Circuit']['Location']['country']
-        locality = race['Circuit']['Location']['locality']
-        race_date = race['date']
+        season_year = season_race_data_json['MRData']['RaceTable']['season']
+        total_races = season_race_data_json['MRData']['total']
 
-        races.append({
-            'round_number': round_number,
-            'race_name': race_name,
-            'circuit_name': circuit_name,
-            'country': country,
-            'locality': locality,
-            'race_date': race_date
-        })
-        
-        print(all_seasons)
+        all_seasons = [{'seasons': season['season']}
+                       for season in total_seasons_data_json['MRData']['SeasonTable']['Seasons']]
 
-    return render_template('index.html', season_year=season_year, total_races=total_races, races=races, seasons=all_seasons)
+        races = []
+        for race in season_race_data_json['MRData']['RaceTable']['Races']:
+            round_number = race['round']
+            race_name = race['raceName']
+            circuit_name = race['Circuit']['circuitName']
+            country = race['Circuit']['Location']['country']
+            locality = race['Circuit']['Location']['locality']
+            race_date = race['date']
+
+            races.append({
+                'round_number': round_number,
+                'race_name': race_name,
+                'circuit_name': circuit_name,
+                'country': country,
+                'locality': locality,
+                'race_date': race_date
+            })
+
+        return render_template('index.html', season_year=season_year, total_races=total_races, races=races, seasons=all_seasons)
+
+    except Exception as e:
+        logging.error(f"An error occurred: {str(e)}")
+        return render_template('error.html', error_message=str(e))
