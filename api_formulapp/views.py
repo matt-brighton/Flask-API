5e3a35a68491bce576b73a43fa9e5e6cae987035
matt-brighton <mat.brighton@hmrc.gov.uk>
2023-12-20 13:37:28 +0000

    Commit 4

--- api_formulapp/views.py
+++ api_formulapp/views.py
@@ -15,25 +15,42 @@ def get_data_from_api(url):
         return json.loads(response.content)
     except requests.RequestException as e:
         logging.error(f"Error fetching data from {url}: {str(e)}")
-        raise  # Propagate the exception for the caller to handle
+        raise
 
 
 @views.route('/', methods=['GET'])
 def index():
     try:
-        season_race_data_json = get_data_from_api(
+        season_race_data = get_data_from_api(
             API_BASE_URL + '2024.json?limit=1000')
-        total_seasons_data_json = get_data_from_api(
+        total_seasons_data = get_data_from_api(
             API_BASE_URL + 'seasons.json?limit=1000')
+        driver_data = get_data_from_api(
+            API_BASE_URL + 'drivers.json?limit=10000'
+        )
 
-        season_year = season_race_data_json['MRData']['RaceTable']['season']
-        total_races = season_race_data_json['MRData']['total']
+        season_year = season_race_data['MRData']['RaceTable']['season']
+        total_races = season_race_data['MRData']['total']
 
-        all_seasons = [{'seasons': season['season']}
-                       for season in total_seasons_data_json['MRData']['SeasonTable']['Seasons']]
+        drivers = []
+        for driver in driver_data['MRData']['DriverTable']['Drivers']:
+            given_name = driver['givenName']
+            family_name = driver['familyName']
+            date_of_birth = driver['dateOfBirth']
+            nationaility = driver['nationality']
+            
+            drivers.append({
+                'given_name': given_name,
+                'family_name': family_name,
+                'date_of_birth': date_of_birth,
+                'nationality': nationaility
+            })
+        
+        years = [{'seasons': season['season']}
+                       for season in total_seasons_data['MRData']['SeasonTable']['Seasons']]
 
         races = []
-        for race in season_race_data_json['MRData']['RaceTable']['Races']:
+        for race in season_race_data['MRData']['RaceTable']['Races']:
             round_number = race['round']
             race_name = race['raceName']
             circuit_name = race['Circuit']['circuitName']
@@ -50,7 +67,8 @@ def index():
                 'race_date': race_date
             })
 
-        return render_template('index.html', season_year=season_year, total_races=total_races, races=races, seasons=all_seasons)
+
+        return render_template('index.html', season_year=season_year, total_races=total_races, races=races, years=years, drivers=drivers)
 
     except Exception as e:
         logging.error(f"An error occurred: {str(e)}")
