0daed35d046ca8524f8770fa917d18dcbae310b7
matt-brighton <matthew.brighton@hmrc.gov.uk>
2023-12-21 14:07:35 +0000

    Commit 6

--- api_formulapp/views.py
+++ api_formulapp/views.py
@@ -1,7 +1,7 @@
 import json
 import logging
 import requests
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 
 views = Blueprint('views', __name__)
 
@@ -16,7 +16,7 @@ def get_data_from_api(url):
     except requests.RequestException as e:
         logging.error(f"Error fetching data from {url}: {str(e)}")
         raise
-    
+
 
 @views.route('/', methods=['GET'])
 def index():
@@ -37,17 +37,17 @@ def index():
             given_name = driver['givenName']
             family_name = driver['familyName']
             date_of_birth = driver['dateOfBirth']
-            nationaility = driver['nationality']
-            
+            nationality = driver['nationality']
+
             drivers.append({
                 'given_name': given_name,
                 'family_name': family_name,
                 'date_of_birth': date_of_birth,
-                'nationality': nationaility
+                'nationality': nationality
             })
-        
+
         years = [{'seasons': season['season']}
-                       for season in total_seasons_data['MRData']['SeasonTable']['Seasons']]
+                 for season in total_seasons_data['MRData']['SeasonTable']['Seasons']]
 
         races = []
         for race in season_race_data['MRData']['RaceTable']['Races']:
@@ -67,13 +67,12 @@ def index():
                 'race_date': race_date
             })
 
-
         return render_template('index.html', season_year=season_year, total_races=total_races, races=races, years=years, drivers=drivers)
 
     except Exception as e:
         logging.error(f"An error occurred: {str(e)}")
         return render_template('error.html', error_message=str(e))
-    
+
 
 @views.route('/get_all_drivers', methods=['GET'])
 def get_all_drivers():
@@ -81,25 +80,23 @@ def get_all_drivers():
         driver_data = get_data_from_api(
             API_BASE_URL + 'drivers.json?limit=10000'
         )
-        
-        drivers = []
+
+        drivers1 = []
         for driver in driver_data['MRData']['DriverTable']['Drivers']:
             given_name = driver['givenName']
             family_name = driver['familyName']
             date_of_birth = driver['dateOfBirth']
-            nationaility = driver['nationality']
-            
-            drivers.append({
+            nationality = driver['nationality']
+
+            drivers1.append({
                 'given_name': given_name,
                 'family_name': family_name,
                 'date_of_birth': date_of_birth,
-                'nationality': nationaility
-            })   
-        
-        return render_template('drivers.html', drivers=drivers)     
+                'nationality': nationality
+            })
+
+        return render_template('drivers.html', drivers=drivers1)
 
     except Exception as e:
         logging.error(f"An error occurred: {str(e)}")
-        return render_template('error.html', error_message=str(e))        
-    
-    
+        return render_template('error.html', error_message=str(e))
