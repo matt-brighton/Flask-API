1602418bbbf4f1f5410936b73550e7badbac5cf3
matt-brighton <matthew.brighton@hmrc.gov.uk>
2024-04-04 11:01:04 +0100

    Refactored functions

--- api_formulapp/views.py
+++ api_formulapp/views.py
@@ -12,10 +12,26 @@ def get_data_from_api(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
-        return json.loads(response.content)
+        return json.loads(response.content)['MRData']
     except requests.RequestException as e:
         logging.error(f"Error fetching data from {url}: {str(e)}")
         raise
+
+
+def process_drivers(driver_data):
+    return [{'given_name': driver['givenName'],
+             'family_name': driver['familyName'],
+             'date_of_birth': driver['dateOfBirth'],
+             'nationality': driver['nationality']} for driver in driver_data['DriverTable']['Drivers']]
+
+
+def process_races(race_data):
+    return [{'round_number': race['round'],
+             'race_name': race['raceName'],
+             'circuit_name': race['Circuit']['circuitName'],
+             'country': race['Circuit']['Location']['country'],
+             'locality': race['Circuit']['Location']['locality'],
+             'race_date': race['date']} for race in race_data['RaceTable']['Races']]
 
 
 @views.route('/', methods=['GET'])
@@ -26,46 +42,15 @@ def index():
         total_seasons_data = get_data_from_api(
             API_BASE_URL + 'seasons.json?limit=1000')
         driver_data = get_data_from_api(
-            API_BASE_URL + 'drivers.json?limit=10000'
-        )
+            API_BASE_URL + 'drivers.json?limit=10000')
 
-        season_year = season_race_data['MRData']['RaceTable']['season']
-        total_races = season_race_data['MRData']['total']
+        season_year = season_race_data['RaceTable']['season']
+        total_races = season_race_data['total']
 
-        drivers = []
-        for driver in driver_data['MRData']['DriverTable']['Drivers']:
-            given_name = driver['givenName']
-            family_name = driver['familyName']
-            date_of_birth = driver['dateOfBirth']
-            nationality = driver['nationality']
-
-            drivers.append({
-                'given_name': given_name,
-                'family_name': family_name,
-                'date_of_birth': date_of_birth,
-                'nationality': nationality
-            })
-
+        drivers = process_drivers(driver_data)
         years = [{'seasons': season['season']}
-                 for season in total_seasons_data['MRData']['SeasonTable']['Seasons']]
-
-        races = []
-        for race in season_race_data['MRData']['RaceTable']['Races']:
-            round_number = race['round']
-            race_name = race['raceName']
-            circuit_name = race['Circuit']['circuitName']
-            country = race['Circuit']['Location']['country']
-            locality = race['Circuit']['Location']['locality']
-            race_date = race['date']
-
-            races.append({
-                'round_number': round_number,
-                'race_name': race_name,
-                'circuit_name': circuit_name,
-                'country': country,
-                'locality': locality,
-                'race_date': race_date
-            })
+                 for season in total_seasons_data['SeasonTable']['Seasons']]
+        races = process_races(season_race_data)
 
         return render_template('index.html', season_year=season_year, total_races=total_races, races=races, years=years, drivers=drivers)
 
@@ -78,24 +63,9 @@ def index():
 def get_all_drivers():
     try:
         driver_data = get_data_from_api(
-            API_BASE_URL + 'drivers.json?limit=10000'
-        )
-
-        drivers1 = []
-        for driver in driver_data['MRData']['DriverTable']['Drivers']:
-            given_name = driver['givenName']
-            family_name = driver['familyName']
-            date_of_birth = driver['dateOfBirth']
-            nationality = driver['nationality']
-
-            drivers1.append({
-                'given_name': given_name,
-                'family_name': family_name,
-                'date_of_birth': date_of_birth,
-                'nationality': nationality
-            })
-
-        return render_template('drivers.html', drivers=drivers1)
+            API_BASE_URL + 'drivers.json?limit=10000')
+        drivers = process_drivers(driver_data)
+        return render_template('drivers.html', drivers=drivers)
 
     except Exception as e:
         logging.error(f"An error occurred: {str(e)}")
@@ -110,27 +80,11 @@ def get_selected_season():
             selected_season_race_data = get_data_from_api(
                 API_BASE_URL + selected_year + '.json?limit=1000')
 
-            season_year = selected_season_race_data['MRData']['RaceTable']['season']
+            season_year = selected_season_race_data['RaceTable']['season']
+            total_races = selected_season_race_data['total']
+            races = process_races(selected_season_race_data)
 
-            races = []
-            for race in selected_season_race_data['MRData']['RaceTable']['Races']:
-                round_number = race['round']
-                race_name = race['raceName']
-                circuit_name = race['Circuit']['circuitName']
-                country = race['Circuit']['Location']['country']
-                locality = race['Circuit']['Location']['locality']
-                race_date = race['date']
-
-                races.append({
-                    'round_number': round_number,
-                    'race_name': race_name,
-                    'circuit_name': circuit_name,
-                    'country': country,
-                    'locality': locality,
-                    'race_date': race_date
-                })
-
-            return jsonify({'races': races, 'season year': season_year})
+            return jsonify({'races': races, 'season_year': season_year, 'total_races': total_races})
 
         except Exception as e:
             logging.error(f"An error occurred: {str(e)}")
