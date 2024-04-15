7a65c72926b6eb72e47abd87ac035941e4091a18
matt-brighton <matthew.brighton@hmrc.gov.uk>
2024-04-15 18:08:13 +0100

    Removed driver dropdown

--- api_formulapp/views.py
+++ api_formulapp/views.py
@@ -18,13 +18,6 @@ def get_data_from_api(url):
         raise
 
 
-def process_drivers(driver_data):
-    return [{'given_name': driver['givenName'],
-             'family_name': driver['familyName'],
-             'date_of_birth': driver['dateOfBirth'],
-             'nationality': driver['nationality']} for driver in driver_data['DriverTable']['Drivers']]
-
-
 @views.route('/', methods=['GET'])
 def index():
     try:
@@ -32,40 +25,24 @@ def index():
             ERGAST_API_BASE_URL + '2024.json?limit=1000')
         all_seasons_data = get_data_from_api(
             ERGAST_API_BASE_URL + 'seasons.json?limit=1000')
-        all_drivers_data = get_data_from_api(
-            ERGAST_API_BASE_URL + 'drivers.json?limit=10000')
         drivers_top_3 = get_data_from_api(
-            ERGAST_API_BASE_URL + '2024/driverStandings.json?limit=3')
+            ERGAST_API_BASE_URL + '2024/driverStandings.json?limit=100000')
         constructor_top_3 = get_data_from_api(
-            ERGAST_API_BASE_URL + '2024/constructorStandings.json?limit=3')
-        race_results_1st = get_data_from_api(
-            ERGAST_API_BASE_URL + '2024/results/1.json?limit=10000')
+            ERGAST_API_BASE_URL + '2024/constructorStandings.json?limit=10000')
+        race_results = get_data_from_api(
+            ERGAST_API_BASE_URL + '2024/results.json?limit=100000000000')
 
         season_year = current_season_race_data['RaceTable']['season']
         total_races = current_season_race_data['total']
 
-        drivers = process_drivers(all_drivers_data)
         all_season_years = [{'seasons': season['season']}
                             for season in all_seasons_data['SeasonTable']['Seasons']]
         drivers_top_3 = drivers_top_3['StandingsTable']
         constructor_top_3 = constructor_top_3['StandingsTable']
-        race_results_1st = race_results_1st['RaceTable']
+        race_results = race_results['RaceTable']
         return render_template('index.html', season_year=season_year, total_races=total_races, years=all_season_years,
-                               drivers=drivers, drivers_top_3=drivers_top_3, constructor_top_3=constructor_top_3,
-                               race_results_1st=race_results_1st)
-    except Exception as e:
-        logging.error(f"An error occurred: {str(e)}")
-        return render_template('error.html', error_message=str(e))
-
-
-@views.route('/get_all_drivers', methods=['GET'])
-def get_all_drivers():
-    try:
-        driver_data = get_data_from_api(
-            ERGAST_API_BASE_URL + 'drivers.json?limit=10000')
-        drivers = process_drivers(driver_data)
-        return render_template('drivers.html', drivers=drivers)
-
+                               drivers_top_3=drivers_top_3, constructor_top_3=constructor_top_3,
+                               race_results=race_results)
     except Exception as e:
         logging.error(f"An error occurred: {str(e)}")
         return render_template('error.html', error_message=str(e))
@@ -78,29 +55,26 @@ def get_selected_season():
             selected_season_year = request.form.get('season_selection')
             selected_season_race_data = get_data_from_api(
                 ERGAST_API_BASE_URL + selected_season_year + '.json?limit=1000')
-            driver_data = get_data_from_api(
-                ERGAST_API_BASE_URL + 'drivers.json?limit=10000')
             total_seasons_data = get_data_from_api(
                 ERGAST_API_BASE_URL + 'seasons.json?limit=1000')
             drivers_top_3 = get_data_from_api(
-                ERGAST_API_BASE_URL + selected_season_year + '/driverStandings.json?limit=3')
+                ERGAST_API_BASE_URL + selected_season_year + '/driverStandings.json?limit=10000')
             constructor_top_3 = get_data_from_api(
-                ERGAST_API_BASE_URL + selected_season_year + '/constructorStandings.json?limit=3')
-            race_results_1st = get_data_from_api(
-                ERGAST_API_BASE_URL + selected_season_year + '/results/1.json?limit=10000')
+                ERGAST_API_BASE_URL + selected_season_year + '/constructorStandings.json?limit=10000')
+            race_results = get_data_from_api(
+                ERGAST_API_BASE_URL + selected_season_year + '/results.json?limit=100000000000')
 
             season_year = selected_season_race_data['RaceTable']['season']
             total_races = selected_season_race_data['total']
-            drivers = process_drivers(driver_data)
             years = [{'seasons': season['season']}
                      for season in total_seasons_data['SeasonTable']['Seasons']]
             drivers_top_3 = drivers_top_3['StandingsTable']
             constructor_top_3 = constructor_top_3['StandingsTable']
-            race_results_1st = race_results_1st['RaceTable']
+            race_results = race_results['RaceTable']
 
             return render_template('index.html', season_year=season_year, total_races=total_races, years=years,
-                                   drivers=drivers, drivers_top_3=drivers_top_3, constructor_top_3=constructor_top_3,
-                                   race_results_1st=race_results_1st)
+                                   drivers_top_3=drivers_top_3, constructor_top_3=constructor_top_3,
+                                   race_results=race_results)
         except Exception as e:
             logging.error(f"An error occurred: {str(e)}")
             return render_template('error.html', error_message=str(e))
